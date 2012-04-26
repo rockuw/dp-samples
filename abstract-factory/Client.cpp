591d01ab3fca1fdf514ffa0e4df6476e6852d82c
Tianlong Wu <rockuw@gmail.com>
2012-04-26 19:19:05 +0800

    some tweak in abstract-factory pattern to explain this pattern more clearly

--- abstract-factory/Client.cpp
+++ abstract-factory/Client.cpp
@@ -3,32 +3,29 @@
 
 using namespace std;
 
+// the produce procedure remains unchanged
+// because of Abstract Factory pattern
+void produce(AbstractFactory *factory){
+    AbstractProduct1 *product1 = factory->createProduct1();
+    AbstractProduct2 *product2 = factory->createProduct2();
+
+    cout << product1->getDescription() << endl;
+    cout << product2->getDescription() << endl;
+
+    delete product1;
+    delete product2;
+}
+
 int main(int argc, char *argv[])
 {
-    AbstractFactory *factory;
-    AbstractProduct1 *product1;
-    AbstractProduct2 *product2;
-
-    factory = new CNFactory();
-    product1 = factory->createProduct1();
-    product2 = factory->createProduct2();
-
-    cout << product1->getDescription() << endl;
-    cout << product2->getDescription() << endl;
+    AbstractFactory *factory = new CNFactory();
+    produce(factory);
 
     delete factory;
-    delete product1;
-    delete product2;
 
     factory = new USFactory();
-    product1 = factory->createProduct1();
-    product2 = factory->createProduct2();
-
-    cout << product1->getDescription() << endl;
-    cout << product2->getDescription() << endl;
+    produce(factory);
 
     delete factory;
-    delete product1;
-    delete product2;
     return 0;
 }
